eb5d15e387b12e543cf974938da9d45058418462
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-07-08 14:47:48 -0300

    checkout messages

--- projects/pedi-ya/src/app/features/order/checkout/checkout.component.ts
+++ projects/pedi-ya/src/app/features/order/checkout/checkout.component.ts
@@ -5,6 +5,7 @@ import * as cartActions from '../cart/cart.actions';
 
 import {
   AppState,
+  NotificationService,
   routeAnimations,
   ROUTE_ANIMATIONS_ELEMENTS,
   selectIsAuthenticated
@@ -18,6 +19,7 @@ export interface Transaction {
 import { selectOrders, State } from '../order.state';
 import { pluck, tap } from 'rxjs/operators';
 import { CartState } from '../cart/cart.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'pedi-ya-order',
@@ -32,15 +34,25 @@ export class CheckoutComponent implements OnInit {
 
   cart$ = this.store.select(selectOrders).pipe(pluck('cart'));
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private notificationService: NotificationService, private translate: TranslateService) { }
 
   ngOnInit(): void {
   }
 
-  displayedColumns = ['name','amount', 'price', 'subtotal'];
-  
+  displayedColumns = ['name', 'amount', 'price', 'subtotal'];
+
   onConfirmOrder() {
     this.store.dispatch(cartActions.resetCart());
+    this.translate.get('pedi-ya.order.checkout.orderConfirmed').subscribe(translation =>
+      this.notificationService.success(translation)
+    );
+  }
+
+  onCancelOrder() {
+    this.store.dispatch(cartActions.resetCart());
+    this.translate.get('pedi-ya.order.checkout.orderCancelled').subscribe(translation =>
+      this.notificationService.info(translation)
+    )
   }
 
 }
