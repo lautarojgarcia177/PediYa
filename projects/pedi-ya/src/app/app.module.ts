99bc2d7113fad69e48c7e4f557ba1c003896a61e
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-07-11 14:10:36 -0300

    pwa

--- projects/pedi-ya/src/app/app.module.ts
+++ projects/pedi-ya/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CoreModule } from './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -17,7 +19,13 @@ import { AppComponent } from './app/app.component';
     CoreModule,
 
     // app
-    AppRoutingModule
+    AppRoutingModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the app is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+      })
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
